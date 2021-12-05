8d104d05d9662b8e94096c54893dad81200fc635
Yashvardhan Gupta <yashvardhang11@gmail.com>
2021-12-05 23:26:43 +0530

    changed contact form/forum submission

--- doubt.js
+++ doubt.js
@@ -8,17 +8,35 @@ items.forEach(item => item.addEventListener('click', toggleAccordion));
 
 var status = localStorage.getItem("status_var");
 console.log(status);
+status = 1;
+
+document.getElementById("question").addEventListener("keyup", function(e) 
+{
+    if (e.keyCode === 13) {
+        document.getElementById("submit").click();
+    }
+})
 
 const pressedButton = document.getElementById("submit");
 pressedButton.addEventListener("click", function (event) {
-  if (status == 1)
+  const question = document.getElementById("question").value;
+  if (question.length > 10)
   {
-    alert("Your doubt has been recorded!");
+    if (status == 1)
+    {
+      alert("Your doubt has been recorded!");
+      document.getElementById("doubt").innerHTML += "<b>Your submitted doubt is:</b> " + question;
+    }
+
+    else
+    {
+      alert("You need to login first!");
+      location.replace("login.html")
+    }
   }
-  
+
   else
   {
-    alert("You need to login first!");
-    location.replace("login.html")
+    alert("The doubt must be at least 10 characters!")
   }
 })
